40c161b57c9ae698de138a4a108606b03a64e836
Conor Canton <conorstreete@gmail.com>
2024-02-07 15:16:44 +0800

    editing quickview

--- src/components/dashboard/recentOrdersQuickView/index.tsx
+++ src/components/dashboard/recentOrdersQuickView/index.tsx
@@ -1,13 +1,22 @@
 import Card from "antd/es/card/Card"
 import { IOrder } from "../../../interfaces"
+import { ContentWrapper, Title } from "./styled"
+
 
 
 
 export const RecentOrdersQuickView: React.FC<{sharedState: IOrder}> = ({sharedState}) => {
     return(
         <Card>
-             Shared State:{sharedState?.id}
-             
+            <Title
+                strong>
+                Order ID: {sharedState?.orderNumber.toString()}
+            </Title>
+            <ContentWrapper>
+                <h1>{sharedState?.products[0].name}</h1>
+            </ContentWrapper>
+
+
         </Card>
     )
 }
---
+++ src/components/dashboard/recentOrdersQuickView/styled.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "antd";
+import styled from "@emotion/styled";
+
+export const Title = styled(Typography.Text)`
+    font-size: 16px;
+    word-break: inherit;
+`;
+
+export const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
