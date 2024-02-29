0af2d101a05893dbc17f29e34443d3a453b09749
Conor Canton <conorstreete@gmail.com>
2024-02-29 18:49:38 +0800

    added impact projects cards

--- src/components/dashboard/impactProjects/index.tsx
+++ src/components/dashboard/impactProjects/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { ImpactProjectCard } from "./impactProjectCard/impactProjectCard";
+import { Col, Row } from "antd";
 
 
 
@@ -7,10 +8,18 @@ import { ImpactProjectCard } from "./impactProjectCard/impactProjectCard";
 export const ImpactProjects: React.FC = () => {
     
     return(
-        <div>
-
-            <ImpactProjectCard></ImpactProjectCard>
-            <ImpactProjectCard></ImpactProjectCard>
+        <div style={{width:'100%',height:'20vh'}}>
+            <Row gutter={[16, 16]} style={{width:'100%',height:'30vh'}}>
+                <Col  xl={8} lg={12} md={24} sm={24} xs={24}>
+                    <ImpactProjectCard></ImpactProjectCard>
+                </Col>
+                <Col  xl={8} lg={12} md={24} sm={24} xs={24}>
+                    <ImpactProjectCard></ImpactProjectCard>
+                </Col>
+                <Col  xl={8} lg={12} md={24} sm={24} xs={24}>
+                    <ImpactProjectCard></ImpactProjectCard>
+                </Col>
+            </Row>
         </div>
         )
 }
