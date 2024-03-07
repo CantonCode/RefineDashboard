656bf42865ab14789f3178a1afb9d0f2cde2a3f0
Conor Canton <conorstreete@gmail.com>
2024-03-07 19:29:58 +0800

    login page remade

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -12,21 +12,28 @@ export const Login: React.FC = () => {
 
         <Layout
             style={{
+                textAlign:'center',
                 height: "100vh",
                 justifyContent: "center",
                 alignItems: "center",
-                background: "radial-gradient(50% 50% at 50% 50%,rgba(255, 255, 255, 0) 0%,rgba(0, 0, 0, 0.5) 100%),url('https://back.3blmedia.com/sites/default/files/styles/ratio_3_2/public/triplepundit/wide/ocean%20plastic%20nrel%20recycling.jpg?h=bc09f3d1')",
+                background: "radial-gradient(50% 50% at 50% 50%,rgba(255, 255, 255, 0) 0%,rgba(0, 0, 0, 0.5) 100%),url('https://grist.org/wp-content/uploads/2022/09/microplastics-grist-copy.png')",
                 backgroundSize: "cover",
+                backgroundPosition:"center center"
             }}
 
         >
 
             <Card 
-            style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', margin:'2.5rem',padding:'10%' }}>
+            style={{ alignItems: 'center', display: 'flex',
+             justifyContent: 'center', margin:'2.5rem',padding:'10%',
+             background:'linear-gradient(to bottom right, rgba(255,255,255,0.2), rgba(255,255,255,0))',
+             backdropFilter:'blur(10px)',
+            boxShadow:'10px 10px 10px rgba(30,30,30,0.5);',
+            border:'none'}}>
                 <Space direction="vertical" align="center">
                     <div style={{ padding: '10px',textAlign:'center' }}>
                         <img src='https://cdn-icons-png.flaticon.com/512/8044/8044414.png' height='50%' width='50%'></img>
-                        <h2>Please Login below</h2>
+                        <h1 style={{color:'white'}}>Please Login below</h1>
                         
                     </div>
                     <LoginButton/>
