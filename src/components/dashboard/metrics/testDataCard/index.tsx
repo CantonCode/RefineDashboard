d284af7a3edca6f809e7adba925a3fe608146438
Conor Canton <conorstreete@gmail.com>
2024-03-08 13:07:49 +0800

    adding icon to dashboard metrics

--- src/components/dashboard/metrics/testDataCard/index.tsx
+++ src/components/dashboard/metrics/testDataCard/index.tsx
@@ -1,7 +1,8 @@
 import { Title } from "@refinedev/antd"
 import { Card, Col, Row, Typography } from "antd"
 import { DataWrapper, Footer, Header, HeaderNumbers, SubHeader } from "./styled"
-import { CaretDownOutlined, CaretUpOutlined } from "@ant-design/icons";
+import { CaretDownOutlined, CaretUpOutlined, CheckCircleTwoTone } from "@ant-design/icons";
+import Icon from "@ant-design/icons/lib/components/Icon";
 
 export const MetricDataCard: React.FC = (metric:any) => {
     const { Text, Title } = Typography;
@@ -11,21 +12,19 @@ export const MetricDataCard: React.FC = (metric:any) => {
                 <SubHeader>
                     <Text>{metric.name}</Text>
                 </SubHeader>
-                <Header>
+                {/* <Header>
+                    <Title>{metric.amount}{metric.unit}</Title>
+                </Header> */}
+                <div>
+                    <Row gutter={[8, 8]}>
+                        <Col span={12} style={{alignItems:'center',display:'flex',justifyContent:'center'}}> <Header>
                     <Title>{metric.amount}{metric.unit}</Title>
                 </Header>
-                {/* <div>
-                    <Row gutter={[8, 8]}>
-                        <Col span={12}> <Text strong>20%</Text>
-                                {(20 ?? 0) > 0 ? (
-                                    <CaretUpOutlined color="green" />
-                                ) : (
-                                    <CaretDownOutlined color="red" />
-                                )}
+
                         </Col>
-                        <Col span={12}><Text>This Month</Text></Col>
+                        <Col span={12} style={{alignItems:'center',display:'flex',justifyContent:'center'}}> <img src="https://cdn-icons-png.flaticon.com/512/7757/7757799.png" width='100px'></img></Col>
                     </Row>
-                </div> */}
+                </div>
                 
             </DataWrapper> 
         </Card>
